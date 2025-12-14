0ef794bccad824e5fb2610fde0d6b9c4a5926852
MRTHOX <muratotunc@gmail.com>
2025-12-14 15:42:07 +0100

    Update AvailableWorkCard.tsx

--- src/components/AvailableWorkCard.tsx
+++ src/components/AvailableWorkCard.tsx
@@ -47,7 +47,7 @@ export default function AvailableWorkCard({ work }: Props) {
           rel="noopener noreferrer"
           className="inline-block mt-3 text-xs tracking-wider hover:text-red-600 transition-colors duration-200"
         >
-          VIEW / COLLECT &rarr;
+          VIEW / COLLECT →
         </a>
       </div>
     </div>
